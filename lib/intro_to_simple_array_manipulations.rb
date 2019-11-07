0e3ffe2989ea917a1ea4975873d9b3934ddedd9c
kowalix007 <kowalix007@gmail.com>
2019-11-07 17:28:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,14 +15,14 @@ def using_unshift (boroughs_in_nyc,new_neighborhood)
  end
  
  def using_pop (continents)
-    continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+   
     deleted_string = continents.pop
     
  end  
  
  def pop_with_args (dog_breeds)
    
-  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua","Shiba Inu"]
+  
   
   small_dogs = dog_breeds.pop(2)
   
@@ -30,7 +30,6 @@ def using_unshift (boroughs_in_nyc,new_neighborhood)
  
  def using_shift (my_favorite_cities)
    
- my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
     
   im_so_over_this_city = my_favorite_cities.shift
   
