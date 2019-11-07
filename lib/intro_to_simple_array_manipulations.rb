9e9cd6762c72e535a566c0c2a5d04341639fee6d
kowalix007 <kowalix007@gmail.com>
2019-11-07 17:06:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -72,10 +72,9 @@ def using_flatten (instruments)
 end
 
 def using_delete (instructors,no_offense_steven) 
-  
-instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda","Steven"]
+  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda","Steven"]
     no_offense_steven = instructors.delete ("Steven")
-instructors = no_offense_steven
+
 end
 
 def using_delete_at (famous_robots,deleted_robots) 
