0ad5697d3cc809eb808b6335e9f1112d8d9445df
kowalix007 <kowalix007@gmail.com>
2019-11-07 17:08:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -48,7 +48,7 @@ def shift_with_args (ice_cream_brands)
     my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
     more_favs = ["sports cars", "flatiron school"]
     all_my_favs = my_favorite_things.concat(more_favs)
-    my_favorite_things.size
+    
   end
    
   def using_insert ( list_of_programming_languages,another_language) 
